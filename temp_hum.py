a7d9dc5a772e7b0843fb209ba4cb7646602caefa
eric <egithub13@gmail.com>
2024-05-14 17:18:24 -0400

    Initial commit. Sensors.py file was used to ensure publish subscribe communication with AWS (MQTT Broker) endpoint.

---
+++ temp_hum.py
@@ -0,0 +1,28 @@
+import Adafruit_DHT
+
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT22
+
+# Example using a Raspberry Pi with DHT sensor
+# connected to GPIO3.
+pin = 3
+def get_data():
+
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+        fahrenheit = (temperature * 9/5) + 32
+        print('Temp={0:0.1f}\u00b0  Humidity={1:0.1f}%'.format(fahrenheit, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
+
+
+
